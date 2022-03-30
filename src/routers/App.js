98b6aeca53f85aae5867c077d2fc17aaa2de87f2
Hana Fitriana <hana.fitriana12@gmail.com>
2022-03-30 14:59:42 -0700

    nambah file konsultasi

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,9 +1,13 @@
-
+import Navbars from '../layouts/Navbars';
 import './App.css';
+import "@material-tailwind/react/tailwind.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbars></Navbars>
+      ga tau sih 
+      jdi gmana?
          </div>
   );
 }
