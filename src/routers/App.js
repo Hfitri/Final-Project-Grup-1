8a8817d491cd0abe19d7f23a6f9c2e13fff4e3f7
mukhyiddin <yudivandiego@gmail.com>
2022-03-30 13:02:09 +0800

    Navbar
    Create Navar

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Navbars from '../layouts/Navbars';
 import './App.css';
+import "@material-tailwind/react/tailwind.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbars />
+      <div data-aos="zoom-in-right">23</div>
+      <div data-aos="zoom-in-right">23</div>
+      <div data-aos="zoom-in-right">23</div>
+      <div data-aos="zoom-in-right">23</div>
+      <div data-aos="zoom-in-right">23</div>
+      <div data-aos="zoom-in-right">23</div>
+      <div data-aos="zoom-in-right">23</div>
+
     </div>
   );
 }
