e35f0c25c55105df3529b7498debb7a931d6a3ac
Hana Fitriana <hana.fitriana12@gmail.com>
2022-04-06 13:09:22 -0700

    add router

--- src/component/TitleKonsultasi.jsx
+++ src/component/TitleKonsultasi.jsx
@@ -12,16 +12,7 @@ function TitleKonsultasi() {
             text-center">
                 <h2> Tanya- tanya, atau curhat ke dokter kucing tapi gak pakai bayar alias gratis? yuk coba konsultasi. </h2>
             </div>
-            <div class="
-            w-full">
-                <h3 class="mb-8 
-                    text-2l 
-                    font-bold 
-                    mt-0 container 
-                    mx-auto 
-                    px-24 
-                    text-center bg-blue-800 bg-opacity-40">Semakin mudah untuk konsultasi secara online dengan dokter hewan kami, Sekarang anda hanya perlu kunjungi LOVELY CAT! Mudah, Aman, Terpercaya</h3>
-            </div>
+            
         </>
     )
 }
---
+++ src/page/NotFound.jsx
@@ -0,0 +1,9 @@
+import H1 from "@material-tailwind/react/Heading1";
+
+const NotFound = () => (
+    <div className="mt-64 mb-96 text-center">
+        <H1 className="text-center">Not Found</H1>
+    </div>
+);
+
+export default NotFound;
